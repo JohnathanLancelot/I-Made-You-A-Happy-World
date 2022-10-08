3a3e0c9497b7d669c2f3c62fd481652e063bbed5
Johnathan Lancelot <johnathan.lancelot@gmail.com>
2022-10-08 19:29:25 +1300

    The sign in the beach room now functions as it should!

---
+++ static/functions.js
@@ -0,0 +1,31 @@
+// The function for when the user presses down while hovering over a sign:
+function showSign(room)
+{
+    // Check which room this occurred in:
+    if (room == "beachRoom")
+    {
+        // Make the beach room's sign appear as a zoomed-in image:
+        document.getElementById("beachSign").style.opacity = "100%";
+    }
+    else if (room == "keyRoom")
+    {
+        // Make the sign inside the hallway with a key appear as a zoomed-in image:
+
+    }
+}
+
+// The function for when the mouse is not being held down when hovering over a sign:
+function removeSign(room)
+{
+    // Check which room the user is in:
+    if (room == "beachRoom")
+    {
+        // Make the beach room's sign disappear:
+        document.getElementById("beachSign").style.opacity = "0%";
+    }
+    else if (room == "keyRoom")
+    {
+        // Make the sign inside the hallway with a key disappear:
+
+    }
+}
