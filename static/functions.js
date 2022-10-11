321230305422819a47904a897b7e391c6ca1233d
Johnathan Lancelot <johnathan.lancelot@gmail.com>
2022-10-11 22:17:16 +1300

    Keys can now be picked up (one in a hallway and one by clicking on the chest in the beach room). The inventory hasn't been added yet, but a zoomed in overlay of the key comes up for 4 seconds when a key is picked up, and a variable (boolean) for each key turns from false to true.

--- static/functions.js
+++ static/functions.js
@@ -1,3 +1,7 @@
+// Global variables:
+var key1Taken = false;
+var key2Taken = false;
+
 // The function for when the user presses down while hovering over a sign:
 function showSign(room)
 {
@@ -69,5 +73,46 @@ function hideNote(room)
 
         // Remove the note pop-up:
         document.getElementById("mirrorRoomNote").style.opacity = "0%";
+    }
+}
+
+// The function for picking up keys:
+function keyTaken(room)
+{
+    if (room == "keyHall" && !key1Taken)
+    {
+        // Make the small key in the background disappear:
+        document.getElementById("keyHallSmallKey").style.opacity = "0%";
+
+        // Change the key taken variable to true:
+        key1Taken = true;
+
+        // Show the pop-up for a short amount of time (4 seconds):
+        document.getElementById("keyHallBigKey").style.opacity = "100%";
+        setTimeout(keyPopRemoved, 4000, "keyHall");
+    }
+    else if (room == "beachRoom" && !key2Taken)
+    {
+        // Change the key taken variable to true:
+        key2Taken = true;
+
+        // Show the key pop-up for 4 seconds:
+        document.getElementById("beachRoomBigKey").style.opacity = "100%";
+        setTimeout(keyPopRemoved, 4000, "beachRoom");
+    }
+}
+
+// The function for removing a key pop-up:
+function keyPopRemoved(room)
+{
+    if (room == "keyHall")
+    {
+        // Make the pop-up disappear:
+        document.getElementById("keyHallBigKey").style.opacity = "0%";
+    }
+    else if (room == "beachRoom")
+    {
+        // Remove the pop-up:
+        document.getElementById("beachRoomBigKey").style.opacity = "0%";
     }
 }
